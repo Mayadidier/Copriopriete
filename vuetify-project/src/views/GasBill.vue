64bde64c5b289ef9740d2bc46d2b1b427da217c0
Mayadidier <maya.didier15@gmail.com>
2023-07-03 22:17:14 +0200

    MAJ 03-07

---
+++ vuetify-project/src/views/GasBill.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-container fluid>
+    <v-col cols="10" offset="2">
+      <v-row no-gutters class="align-center justify-center">
+        <h1>Facture Gaz</h1>
+      </v-row>
+    </v-col>
+
+    <v-col offset="3" cols="8">
+      <v-card class="mx-auto px-6 py-8" max-width="600">
+        <v-form v-model="form" @submit="checkForm" method="post">
+          <v-col cols=" mx-auto">
+            Voir pour date
+            <v-text-field type="date" label="Date"></v-text-field>
+
+            <v-text-field
+              v-model="Montant"
+              :readonly="loading"
+              variant="solo"
+              suffix="€"
+              clearable
+              class="mb-2"
+              label="Montant HT"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="Montant"
+              :readonly="loading"
+              variant="solo"
+              suffix="€"
+              clearable
+              class="mb-2"
+              label="Abonnement"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="Contributions"
+              :readonly="loading"
+              variant="solo"
+              suffix="€"
+              clearable
+              class="mb-2"
+              label="Contributions"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="Taxe"
+              :readonly="loading"
+              variant="solo"
+              suffix="€"
+              clearable
+              class="mb-2"
+              label="Taxe"
+            ></v-text-field>
+
+            <br />
+
+            <v-btn
+              :disabled="!form"
+              :loading="loading"
+              block
+              color="success"
+              type="submit"
+              value="Submit"
+              variant="elevated"
+            >
+              Valider
+            </v-btn>
+          </v-col>
+        </v-form>
+      </v-card>
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+import Menu from "../components/Menu/Menu.vue";
+// Déclaration du composant Vue
+export default {
+  data() {
+    return {
+      releves: ["Montant HT", "Abonnement", "Contribution", "Taxe"],
+      rules: {
+        required: (value) => !!value || "Champs manquant",
+        number: (value) => !isNaN(value) || "Veuillez rentrer un nombre",
+      },
+    };
+  },
+  components: {
+    Menu,
+  },
+};
+</script>
+
+<script setup></script>
+
+<!-- Utilise le langage SCSS https://sass-lang.com/guide -->
+<style lang="scss"></style>
